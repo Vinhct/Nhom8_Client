f4af77f501e6515540d09ff103eac58733f7b335
TienManh <manhtienmai21112003@gmail.com>
2024-10-26 00:04:57 +0700

    Add to card và giỏ hàng

--- app/(root)/products/[productId]/page.tsx
+++ app/(root)/products/[productId]/page.tsx
@@ -14,12 +14,7 @@ const ProductDetails = async ({ params }: { params: { productId: string } }) => 
                 <ProductInfo productInfo={productDetails} />
             </div>
 
-            <div className="flex flex-col items-center px-10 py-5 max-md:px-3">
-                <p className="text-heading3-bold">Sản phẩm liên quan</p>
-                <div className="flex flex-wrap gap-16 mx-auto mt-8">
-                    
-                </div>
-            </div>
+
         </>
     )
 }
