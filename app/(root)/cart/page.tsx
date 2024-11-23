1b66e20d17029e580d47e3e4aaab108bf7b40962
TienManh <manhtienmai21112003@gmail.com>
2024-11-23 12:45:41 +0700

    Test commit

--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -75,13 +75,9 @@ const Cart = () => {
                 </div>
 
                 <div className="flex gap-4 items-center">
-                  <MinusCircle
+                <MinusCircle
                     className="hover:text-red-1 cursor-pointer"
-                    onClick={() => {
-                      if (cartItem.quantity > 1) {
-                        cart.decreaseQuantity(cartItem.item._id);
-                      }
-                    }}
+                    onClick={() => cart.decreaseQuantity(cartItem.item._id)}
                   />
                   <p className="text-body-bold">{cartItem.quantity}</p>
                   <PlusCircle
